4b97ece7e1426a22842445d7b440d6d17363f653
psg1415 <psg1415@naver.com>
2021-07-23 17:12:18 +0900

    nodejs 학습:

---
+++ exam/file/async_old.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+//비동기 순차 처리
+fs.readFile('./writeme.txt', (err, data) => {
+		console.log("1번", data.toString());
+		fs.readFile("./writeme.txt", (err, data) => {
+			console.log("2번", data.toString());
+			fs.readFile("./writeme.txt", (err, data) => {
+				console.log("3번", data.toString());
+				fs.readFile("./writeme.txt", (err, data) => {
+					console.log("4번", data.toString());
+					fs.readFile("./writeme.txt", (err, data) => {
+						console.log("5번", data.toString());
+					});
+				});
+			});
+		});
+});
+
+
+
+/* 비동기 - 끝나는 결과 순서가 유지 X
+fs.readFile('./writeme.txt', (err, data) => {
+	console.log("1번", data.toString());
+});
+fs.readFile('./writeme.txt', (err, data) => {
+	console.log("2번", data.toString());
+});
+fs.readFile('./writeme.txt', (err, data) => {
+	console.log("3번", data.toString());
+});
+fs.readFile('./writeme.txt', (err, data) => {
+	console.log("4번", data.toString());
+});
+fs.readFile('./writeme.txt', (err, data) => {
+	console.log("5번", data.toString());
+});
+*/
